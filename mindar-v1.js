9daf445bf28b907e4f27af2db3df95642a87fa47
Din9-max <61575753+Din9-max@users.noreply.github.com>
2026-01-13 03:45:38 +0700

    Update mindar-v1.js

--- mindar-v1.js
+++ mindar-v1.js
@@ -1,2 +1,3 @@
-// MindAR v1.1.5
-import('https://unpkg.com/mind-ar@1.1.5/dist/mindar-image-aframe.prod.js');
+var script = document.createElement('script');
+script.src = 'https://cdn.bootcdn.net/ajax/libs/mind-ar/1.1.5/mindar-image-aframe.prod.js';
+document.head.appendChild(script);
