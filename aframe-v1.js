cfdc496273324d886cb2b4fe369da3b4e1d080a3
Din9-max <61575753+Din9-max@users.noreply.github.com>
2026-01-13 03:45:20 +0700

    Update aframe-v1.js

--- aframe-v1.js
+++ aframe-v1.js
@@ -1,2 +1,3 @@
-// A-Frame v1.2.0
-import('https://unpkg.com/aframe@1.2.0/dist/aframe-master.min.js');
+var script = document.createElement('script');
+script.src = 'https://cdn.bootcdn.net/ajax/libs/aframe/1.2.0/aframe.min.js';
+document.head.appendChild(script);
