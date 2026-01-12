3ce13c502e7b07e27b3eaa1234989defbd7f76e1
Din9-max <61575753+Din9-max@users.noreply.github.com>
2026-01-13 03:42:54 +0700

    Update mindar-v1.js

--- mindar-v1.js
+++ mindar-v1.js
@@ -1,1 +1,2 @@
-document.write('<script src="https://unpkg.com/mind-ar@1.1.5/dist/mindar-image-aframe.prod.js"><\/script>');
+// MindAR v1.1.5
+import('https://unpkg.com/mind-ar@1.1.5/dist/mindar-image-aframe.prod.js');
